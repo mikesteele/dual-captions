8080a922976e94fb89ca68137efa1717ddb25a19
Mike Steele <mike@steel.fm>
2018-01-21 18:05:22 -0500

    Minor changes

--- background.js
+++ background.js
@@ -1,14 +1,17 @@
 // Tell pop-up to re-inject libraries if tab changes status/url 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   if (changeInfo.status) {
-    setValueInStorage(`DUAL_CAPTIONS-librariesLoaded-${tabId}`, false);
+    setValueInStorage(`DUAL_CAPTIONS-librariesLoaded-${tabId}`, false)
+      .then(() => {});
   }
 });
 
-function setValueInStorage(key, value, callback) {
-  let obj = {};
-  obj[key] = value;
-  chrome.storage.local.set(obj, function() {
-  	if (callback) callback();
+function setValueInStorage(key, value) {
+  return new Promise((resolve, _) => {
+    let obj = {};
+    obj[key] = value;
+    chrome.storage.local.set(obj, () => {
+      resolve();
+    });
   });
 }
