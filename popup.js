f3d795aecf14d56398d372f086ebc837d12efd93
Mike Steele <mike@steel.fm>
2018-01-21 15:05:44 -0500

    Popup reflects libariesLoaded

--- popup.js
+++ popup.js
@@ -3,7 +3,9 @@ function loadLibraries() {
     chrome.tabs.executeScript(null, {file: "lib/querystring-encode.js"}, function() {
       chrome.tabs.executeScript(null, {file: "lib/google-translate-api.js"}, function() {
         chrome.tabs.executeScript(null, {file: "lib/dual-captions.js"}, function() {
-          setValueInStorage('DUAL_CAPTIONS-librariesLoaded', true);
+          setValueInStorage('DUAL_CAPTIONS-librariesLoaded', true, function() {
+
+          });
         });
       });
     });
@@ -25,10 +27,41 @@ function setValueInStorage(key, value, callback) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+var buttons = ['load-button', 'on-button', 'off-button'];
+function showButton(buttonType) {
+  buttons.forEach(buttonId => {
+    if (buttonType === buttonId) {
+      document.getElementById(buttonId).removeAttribute('hidden');
+    } else {
+      document.getElementById(buttonId).setAttribute('hidden', true);
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
   getValueInStorage('DUAL_CAPTIONS-librariesLoaded', (librariesLoaded) => {
     if (!librariesLoaded) {
-      loadLibraries();
+      showButton('load-button');
+    } else {
+      showButton('on-button');
     }
+  });
+  document.getElementById('load-button').addEventListener('click', () => {
+    getValueInStorage('DUAL_CAPTIONS-librariesLoaded', (librariesLoaded) => {
+      if (!librariesLoaded) {
+        loadLibraries();
+      }
+      showButton('on-button');
+    });
+  });
+  document.getElementById('on-button').addEventListener('click', () => {
+    // TODO
+    // 1. Stop observer
+    showButton('off-button');
+  });
+  document.getElementById('off-button').addEventListener('click', () => {
+    // TODO
+    // 1. Restart observer
+    showButton('on-button');
   });
 });
