82a54c5342c8c217792b660f98f59cbafdcff167
Mike Steele <mike@steel.fm>
2018-01-21 18:20:51 -0500

    Add all supported languages

--- popup.js
+++ popup.js
@@ -1,5 +1,119 @@
 let activeTabId;
 
+const BUTTON_IDS = ['load-button', 'on-button', 'off-button'];
+/**
+ *
+ * List from https://github.com/matheuss/google-translate-api/blob/master/languages.js
+ *
+ */
+const SUPPORTED_LANGAUGES = {
+  'auto': 'Detect Language',
+  'af': 'Afrikaans',
+  'sq': 'Albanian',
+  'am': 'Amharic',
+  'ar': 'Arabic',
+  'hy': 'Armenian',
+  'az': 'Azerbaijani',
+  'eu': 'Basque',
+  'be': 'Belarusian',
+  'bn': 'Bengali',
+  'bs': 'Bosnian',
+  'bg': 'Bulgarian',
+  'ca': 'Catalan',
+  'ceb': 'Cebuano',
+  'ny': 'Chichewa',
+  'zh-cn': 'Chinese Simplified',
+  'zh-tw': 'Chinese Traditional',
+  'co': 'Corsican',
+  'hr': 'Croatian',
+  'cs': 'Czech',
+  'da': 'Danish',
+  'nl': 'Dutch',
+  'en': 'English',
+  'eo': 'Esperanto',
+  'et': 'Estonian',
+  'tl': 'Filipino',
+  'fi': 'Finnish',
+  'fr': 'French',
+  'fy': 'Frisian',
+  'gl': 'Galician',
+  'ka': 'Georgian',
+  'de': 'German',
+  'el': 'Greek',
+  'gu': 'Gujarati',
+  'ht': 'Haitian Creole',
+  'ha': 'Hausa',
+  'haw': 'Hawaiian',
+  'iw': 'Hebrew',
+  'hi': 'Hindi',
+  'hmn': 'Hmong',
+  'hu': 'Hungarian',
+  'is': 'Icelandic',
+  'ig': 'Igbo',
+  'id': 'Indonesian',
+  'ga': 'Irish',
+  'it': 'Italian',
+  'ja': 'Japanese',
+  'jw': 'Javanese',
+  'kn': 'Kannada',
+  'kk': 'Kazakh',
+  'km': 'Khmer',
+  'ko': 'Korean',
+  'ku': 'Kurdish (Kurmanji)',
+  'ky': 'Kyrgyz',
+  'lo': 'Lao',
+  'la': 'Latin',
+  'lv': 'Latvian',
+  'lt': 'Lithuanian',
+  'lb': 'Luxembourgish',
+  'mk': 'Macedonian',
+  'mg': 'Malagasy',
+  'ms': 'Malay',
+  'ml': 'Malayalam',
+  'mt': 'Maltese',
+  'mi': 'Maori',
+  'mr': 'Marathi',
+  'mn': 'Mongolian',
+  'my': 'Myanmar (Burmese)',
+  'ne': 'Nepali',
+  'no': 'Norwegian',
+  'ps': 'Pashto',
+  'fa': 'Persian',
+  'pl': 'Polish',
+  'pt': 'Portuguese',
+  'ma': 'Punjabi',
+  'ro': 'Romanian',
+  'ru': 'Russian',
+  'sm': 'Samoan',
+  'gd': 'Scots Gaelic',
+  'sr': 'Serbian',
+  'st': 'Sesotho',
+  'sn': 'Shona',
+  'sd': 'Sindhi',
+  'si': 'Sinhala',
+  'sk': 'Slovak',
+  'sl': 'Slovenian',
+  'so': 'Somali',
+  'es': 'Spanish',
+  'su': 'Sundanese',
+  'sw': 'Swahili',
+  'sv': 'Swedish',
+  'tg': 'Tajik',
+  'ta': 'Tamil',
+  'te': 'Telugu',
+  'th': 'Thai',
+  'tr': 'Turkish',
+  'uk': 'Ukrainian',
+  'ur': 'Urdu',
+  'uz': 'Uzbek',
+  'vi': 'Vietnamese',
+  'cy': 'Welsh',
+  'xh': 'Xhosa',
+  'yi': 'Yiddish',
+  'yo': 'Yoruba',
+  'zu': 'Zulu'
+};
+
 function getActiveTabId() {
   return new Promise((resolve, reject) => {
     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
@@ -16,7 +130,7 @@ function getActiveTabId() {
   });
 }
 
-function initalizeUI() {
+function initializeButton() {
   return new Promise((resolve, reject) => {
     Promise.all([
       getValueInStorage(`DUAL_CAPTIONS-librariesLoaded-${activeTabId}`),
@@ -38,7 +152,23 @@ function initalizeUI() {
   });
 }
 
-const BUTTON_IDS = ['load-button', 'on-button', 'off-button'];
+function initializeSelects() {
+  return new Promise((resolve, reject) => {
+    const fromLangaugeSelect = document.getElementById('from-select');
+    const toLangaugeSelect = document.getElementById('to-select');
+    for (lang in SUPPORTED_LANGAUGES) {
+      let option = document.createElement('option');
+      option.value = lang;
+      option.label = SUPPORTED_LANGAUGES[lang];
+      fromLangaugeSelect.appendChild(option);
+      if (option.value !== 'auto') {
+        toLangaugeSelect.appendChild(option.cloneNode());
+      }
+    }
+    resolve();
+  });
+}
+
 function showButton(button) {
   BUTTON_IDS.forEach(buttonId => {
     if (button === buttonId) {
@@ -158,8 +288,8 @@ function setValueInStorage(key, value) {
 
 document.addEventListener('DOMContentLoaded', () => {
   getActiveTabId()
-    .then(initalizeUI)
+    .then(initializeButton)
+    .then(initializeSelects)
     .then(setListeners)
     .catch(() => {}); // TODO
 });
-
