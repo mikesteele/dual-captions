bf7079848b4e9e7a40333f0a3b67270b34be07e1
Mike Steele <mike@steel.fm>
2018-02-28 21:45:44 -0500

    Rename

--- popup.js
+++ popup.js
@@ -1,8 +1,31 @@
 /**
  *
  * Dual Captions
+ * <3 Mike Steele 2018
+ * License: MIT
  * 
  */
+
+/**
+ *
+ * Chrome Utils
+ *
+ */
+
+function getActiveTabId() {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({
+      currentWindow: true,
+      active: true
+    }, tabs => {
+      if (tabs && tabs.length > 0) {
+        resolve(tabs[0].id);
+      } else {
+        reject('Could not get active tab ID.');
+      }
+    });
+  });
+}
 
 /**
  *
@@ -10,31 +33,77 @@
  *
  */
 
-function setObserverLanguage(language) {
+function isObserverOn() {
   return new Promise((resolve, _) => {
-    chrome.tabs.query({currentWindow: true,active: true}, function(tabs){
-      chrome.tabs.sendMessage(tabs[0].id, {
-        type: 'change-language',
-        info: {
-          lang: language
-        }
-      });
-    });
+    getActiveTabId()
+      .then(tabId => {
+        chrome.tabs.sendMessage(tabId, {
+          type: 'is-on'
+        }, resolve);
+      })
+      .catch(() => {});
   });
 }
 
-function getState() {
+function getObserverLanguage(language) {
   return new Promise((resolve, _) => {
-    Promise.all([
-      getObserverLanguage()
-      //
-      // TODO - Eventually add here queries for options like extra space, two colors, etc. 
-      //
-    ]).then(values => {
-      const language = values[0];
-      if (language) {
-        setLanguageSelect(language);
-      }
+    getActiveTabId()
+      .then(tabId => {
+        chrome.tabs.sendMessage(tabId, {
+          type: 'get-language',
+        }, response => {
+          resolve(response);
+        });
+      })
+      .catch(() => {});
+  });
+}
+
+
+function setObserverLanguage(language) {
+  return new Promise((resolve, _) => {
+    getActiveTabId()
+      .then(tabId => {
+        chrome.tabs.sendMessage(tabId, {
+          type: 'change-language',
+          info: {
+            lang: language
+          }
+        }, response => {
+          resolve(response);
+        });
+      })
+      .catch(() => {});
+  });
+}
+
+function startObserver() {
+  return new Promise((resolve, _) => {
+    getActiveTabId()
+      .then(tabId => {
+        chrome.tabs.sendMessage(tabId, {
+          type: 'start-observer'
+        }, resolve);
+      })
+      .catch(() => {});
+  });
+}
+
+function stopObserver() {
+  return new Promise((resolve, _) => {
+    getActiveTabId()
+      .then(tabId => {
+        chrome.tabs.sendMessage(tabId, {
+          type: 'stop-observer'
+        }, resolve);
+      })
+      .catch(() => {});
+  });
+}
+
+function navigateToGitHubPage() {
+  return new Promise((resolve, _) => {
+    chrome.tabs.executeScript(null, {code: `window.location.replace('https://github.com/mikesteele/dual-captions/')`}, () => {
       resolve();
     });
   });
@@ -42,39 +111,9 @@ function getState() {
 
 function navigateToIssuesPage() {
   return new Promise((resolve, _) => {
-    chrome.tabs.executeScript(null, {code: `window.location.replace("https://github.com/mikesteele/dual-captions/issues")`}, () => {
+    chrome.tabs.executeScript(null, {code: `window.location.replace('https://github.com/mikesteele/dual-captions/issues')`}, () => {
       resolve();
     });
-  });
-}
-
-function loadLibraries() {
-  return new Promise((resolve, _) => {
-    chrome.tabs.executeScript(null, {file: "lib/google-translate-token.js"}, () => {
-      chrome.tabs.executeScript(null, {file: "lib/querystring-encode.js"}, () => {
-        chrome.tabs.executeScript(null, {file: "lib/google-translate-api.js"}, () => {
-          chrome.tabs.executeScript(null, {file: "lib/dual-captions.js"}, resolve);
-        });
-      });
-    });
-  });
-}
-
-function startObserver() {
-  return new Promise((resolve, _) => {
-    chrome.tabs.executeScript(null, {code: `window.DUAL_CAPTIONS.startObserver()`}, resolve);
-  });
-}
-
-function stopObserver() {
-  return new Promise((resolve, _) => {
-    chrome.tabs.executeScript(null, {code: `window.DUAL_CAPTIONS.stopObserver()`}, resolve);
-  });
-}
-
-function setExtraSpace(value) {
-  return new Promise((resolve, _) => {
-    chrome.tabs.executeScript(null, {code: `window.DUAL_CAPTIONS.setExtraSpace(${value})`}, resolve);
   });
 }
 
@@ -103,67 +142,57 @@ function initializeLanguageSelect() {
       option.label = SUPPORTED_LANGUAGES[lang];
       languageSelect.appendChild(option);
     }
-    resolve();
+    getObserverLanguage()
+      .then(language => {
+        languageSelect.value = language;
+        resolve();
+      });
   });
 }
 
-function showSteps(steps) {
-  steps.forEach(step => {
-    let shadow = document.querySelector(`.step-shadow[data-step='${step}']`);
-    if (shadow) {
-      shadow.classList.add('hidden');
-    }
-  });
-}
+function showButton(button) {
+  let onButton = document.getElementById('turn-on-button');
+  let offButton = document.getElementById('turn-off-button');
 
-function hideSteps(steps) {
-  steps.forEach(step => {
-    let shadow = document.querySelector(`.step-shadow[data-step='${step}']`);
-    if (shadow) {
-      shadow.classList.remove('hidden');
-    }
-  });
+  if (button === 'on-button') {
+    offButton.classList.add('hidden');
+    onButton.classList.remove('hidden');
+  } else {
+    onButton.classList.add('hidden');
+    offButton.classList.remove('hidden');
+  }
 }
 
 function showStatus(status) {
   let onStatus = document.getElementById('on-status');
   let offStatus = document.getElementById('off-status');
-  let errorStatus = document.getElementById('error-status');
 
   if (status === 'on') {
-    errorStatus.classList.add('hidden');
     offStatus.classList.add('hidden');
     onStatus.classList.remove('hidden');
-    showIcon('icon-on');
-  } else if (status === 'off') {
-    errorStatus.classList.add('hidden');
+    //showIcon('icon-on');
+  } else {
     onStatus.classList.add('hidden');
     offStatus.classList.remove('hidden');
-    showIcon('icon-off');
-  } else {
-    onStatus.classList.add('hidden');
-    offStatus.classList.add('hidden');
-    errorStatus.classList.remove('hidden');
-    showIcon('icon-off');
+    //showIcon('icon-off');
   }
 }
 
-function showButton(button) {
-  let turnOnButton = document.getElementById('turn-on-button');
-  let turnOffButton = document.getElementById('turn-off-button');
-
-  if (button === 'turn-on') {
-    turnOffButton.classList.add('hidden');
-    turnOnButton.classList.remove('hidden');
+function setStatus(isOn) {
+  if (isOn) {
+    showStatus('on');
+    showButton('off-button');
+    showIcon('icon-on');
   } else {
-    turnOnButton.classList.add('hidden');
-    turnOffButton.classList.remove('hidden');
+    showStatus('off');
+    showButton('on-button');
+    showIcon('icon-off');
   }
-}
-
-function setLanguageSelect(language) {
   const languageSelect = document.getElementById('language-select');
-  languageSelect.value = language;
+  getObserverLanguage()
+    .then(language => {
+      languageSelect.value = language;
+    });
 }
 
 function setListeners() {
@@ -185,39 +214,33 @@ function setListeners() {
         });
     });
 
+    const githubLink = document.getElementById('github-link');
+    githubLink.addEventListener('click', e => {
+      e.preventDefault();
+      navigateToGitHubPage()
+        .then(() => {
+          window.close();
+        });
+    });
+
     const turnOffButton = document.getElementById('turn-off-button');
     turnOffButton.addEventListener('click', e => {
+      e.preventDefault();
       stopObserver()
-        .then(isDualCaptionsOn)
+        .then(isObserverOn)
         .then(isOn => {
-          if (isOn) {
-            showStatus('error');
-          } else {
-            showStatus('off');
-            showButton('turn-on');
-          }
+          setStatus(isOn);
         });
     });
 
     const turnOnButton = document.getElementById('turn-on-button');
     turnOnButton.addEventListener('click', e => {
+      e.preventDefault();
       startObserver()
-        .then(isDualCaptionsOn)
+        .then(isObserverOn)
         .then(isOn => {
-          if (isOn) {
-            showStatus('on');
-            showButton('turn-off');
-          } else {
-            showStatus('error');
-          }
+          setStatus(isOn);
         });
-    });
-
-
-    const extraSpace = document.getElementById('extra-space');
-    extraSpace.addEventListener('click', e => {
-      setExtraSpace(extraSpace.checked)
-        .then(() => {});
     });
 
     resolve();
@@ -233,7 +256,8 @@ function setListeners() {
 document.addEventListener('DOMContentLoaded', () => {
   initializeLanguageSelect()
     .then(setListeners)
-    .then(isLoaded => {
-        showSteps(['step-1', 'step-2', 'step-3', 'step-4']);
+    .then(isObserverOn)
+    .then(isOn => {
+      setStatus(isOn);
     });
 });
