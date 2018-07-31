801bd00ca2f2a1ddc94da60b3a22390affddc382
Mike Steele <misteele@athenahealth.com>
2018-07-31 17:39:53 -0400

    Adapter tests passing

--- src/tests/youtube-adapter.test.js
+++ src/tests/youtube-adapter.test.js
@@ -1,12 +1,9 @@
 import expect from 'expect';
 const fs = require('fs');
 const path = require('path');
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
 
 const videoPage = fs.readFileSync(path.resolve(__dirname, './assets/youtube/video.html'));
-const dom = new JSDOM(videoPage);
-let document = dom.window.document;
+document.body.innerHTML = videoPage;
 
 import '../../public/content-scripts/init';
 import '../../public/content-scripts/config/init';
@@ -16,5 +13,5 @@ const adapter = window.DC.config;
 
 it('should get player element', () => {
   const player = adapter.getPlayer();
-  expect(player.classList.contains('html5-video-player')).to.be.true;
+  expect(player.classList.contains('html5-video-player')).toEqual(true);
 });
