b5859f75805d4cdd4551a6071df2fe3060b1da6a
Mike Steele <mike@steel.fm>
2019-08-20 21:33:26 -0400

    Update README

---
+++ build-extension.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Clean
+rm -rf build/
+
+# Build browser_action
+cd browser_action
+yarn
+yarn build
+
+# Build content_script
+cd ..
+cd content_script
+yarn
+yarn build
+
+# Create extension
+cd ..
+mkdir build
+cp -r browser_action/build/* build/
+cp content_script/build/static/js/main.js build/bundle.js
+cp content_script/build/background.js build/background.js
+cp content_script/build/manifest.json build/manifest.json
