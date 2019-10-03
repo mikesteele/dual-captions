d407f5fd5a9d22230fe787f8d43dc29f9c37dc4d
Mike Steele <mike@steel.fm>
2019-10-02 23:51:28 -0400

    Add zh-tw to translate

--- content_script/src/utils/translate.js
+++ content_script/src/utils/translate.js
@@ -1,8 +1,11 @@
 const en = require('dual-captions-browser-action/build/locales/en/translations.json');
 const fr = require('dual-captions-browser-action/build/locales/fr/translations.json');
+const zhTw = require('dual-captions-browser-action/build/locales/zh-tw/translations.json');
+
 const locales = {
   en,
-  fr
+  fr,
+  'zh-tw': zhTw
 };
 
 const translate = (lang, key) => {
