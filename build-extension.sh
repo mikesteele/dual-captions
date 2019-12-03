2991e2dbae218dfea6231cc9fb72e10c3a7b4318
Mike Steele <mike@steel.fm>
2019-12-02 19:37:09 -0500

    Add skeleton

--- build-extension.sh
+++ build-extension.sh
@@ -3,7 +3,26 @@
 # Clean
 rm -rf build/
 
+# Build site_integrations
+cd site_integrations
+yarn
+yarn build
+yarn link
+
+# Build background_page
+cd ..
+cd background_page
+yarn
+yarn build
+
+# Build manifest
+cd ..
+cd manifest_creator
+yarn
+yarn build
+
 # Build browser_action
+cd ..
 cd browser_action
 yarn
 yarn build
---
+++ manifest_creator/index.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+
+fs.writeFileSync('./manifest.json', JSON.stringify({}));
