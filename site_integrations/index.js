6225549642b40e6ddade07374dea69ce98a7d7f0
Mike Steele <mike@steel.fm>
2020-01-05 23:43:49 -0500

    SrtParser => next release (#123)

--- site_integrations/index.js
+++ site_integrations/index.js
@@ -2,6 +2,7 @@ const NetflixIntegration = require('./netflix');
 const YouTubeIntegration = require('./youtube');
 const DevelopmentIntegration = require('./development');
 const KanopyIntegration = require('./kanopy');
+const SrtParser = require('./shared/srt-parser');
 const SrtEncoder = require('./shared/srt-encoder');
 
 module.exports = {
@@ -13,5 +14,8 @@ module.exports = {
   ],
   encoders: {
     SrtEncoder
+  },
+  parsers: {
+    SrtParser
   }
 };
