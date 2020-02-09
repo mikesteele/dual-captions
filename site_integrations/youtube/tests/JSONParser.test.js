4fb103d0a1f9d0340e1326b316a50a67dcea490e
Mike Steele <mike@steel.fm>
2020-02-09 13:42:39 -0500

    Finish test

--- site_integrations/youtube/tests/JSONParser.test.js
+++ site_integrations/youtube/tests/JSONParser.test.js
@@ -8,7 +8,10 @@ describe('YouTubeJSONParser', () => {
     const captionFile = fs.readFileSync(path.resolve(__dirname, './assets/2-9-20.txt'));
     YouTubeJSONParser(captionFile)
       .then(captions => {
-        console.error(captions);
+        expect(captions.length).to.equal(134);
+        expect(captions[5].text).to.equal('Excuse me?');
+        expect(captions[5].startTime).to.equal(38.137);
+        expect(captions[5].endTime).to.equal(39.085);
         done();
       })
       .catch(err => {
