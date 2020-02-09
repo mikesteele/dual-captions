c6903f4aedf4754569f85a045c4650bf5f5bbd89
Mike Steele <mike@steel.fm>
2020-02-09 13:29:31 -0500

    Looks like YouTube retroactively changed all subtitle files

--- site_integrations/youtube/tests/JSONParser.test.js
+++ site_integrations/youtube/tests/JSONParser.test.js
@@ -4,14 +4,15 @@ const path = require('path');
 const YouTubeJSONParser = require('../parsers/JSONParser');
 
 describe('YouTubeJSONParser', () => {
-  it('should correctly parse file', () => {
+  it('should correctly parse file', done => {
     const captionFile = fs.readFileSync(path.resolve(__dirname, './assets/2-9-20.txt'));
     YouTubeJSONParser(captionFile)
       .then(captions => {
         console.error(captions);
+        done();
       })
       .catch(err => {
         console.error(err);
-      })
+      });
   });
 });
