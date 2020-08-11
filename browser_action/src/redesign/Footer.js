9fefa61cd99d27fafb0497cba3f2f3673fed97d7
Mike Steele <mike@steel.fm>
2020-08-10 21:33:19 -0400

    ideas

--- browser_action/src/redesign/Footer.js
+++ browser_action/src/redesign/Footer.js
@@ -1,12 +1,7 @@
 import React, { Fragment } from 'react';
 import { List, Picker, NoticeBar, Icon } from 'antd-mobile';
 
-const Footer = () => (
-  <Fragment>
-    <NoticeBar mode="link" icon={"✨"} onClick={() => alert('1')}>
-      What's new in v2.3.4?
-    </NoticeBar>
-  </Fragment>
+const Footer = () => (<div>todo</div>
 );
 
 export default Footer;
--- browser_action/src/redesign/MainPage.js
+++ browser_action/src/redesign/MainPage.js
@@ -1,180 +1,92 @@
-import React, { Fragment } from 'react';
-import ReactDOM from 'react-dom';
-import { Tabs, WhiteSpace, Badge, Modal, List, Switch, Picker } from 'antd-mobile';
+import React from 'react';
+import { css } from 'emotion';
+import Toggle from 'react-toggle';
 
-const Fade = props => (
-  <div
-    style={{
-      filter: props.in ? 'opacity(1)' : 'opacity(0)',
-      transition: 'filter 200ms',
-      pointerEvents: props.in ? 'auto' : 'none',
-    }}
-  >
-    { props.children }
+import 'react-toggle/style.css';
+
+const wrapper = css`
+  width: 280px;
+  height: 600px;
+  background: white;
+`;
+
+const header = css`
+  display: flex;
+  justify-content: center;
+  font-size: 12px;
+  line-height: 12px;
+  padding: 8px;
+`;
+
+const controls = css`
+  display: flex;
+  flex-direction: column;
+`;
+
+const controlWrapper = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #aaa;
+  padding: 8px;
+  height: 32px;
+
+  &:first-child {
+    border-top: 1px solid #aaa;
+  }
+`;
+
+const controlLabel = css`
+  font-size: 12px;
+  line-height: 12px;
+  padding: 0;
+`;
+
+const flexbox = css`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+`;
+
+const MainPage = () => (
+  <div className={wrapper}>
+    <div className={header}>
+      Dual Captions v2.1
+    </div>
+    <div className={controls}>
+      <div className={controlWrapper}>
+        <div className={controlLabel}>On</div>
+        <Toggle />
+      </div>
+      <div className={controlWrapper}>
+        <div className={controlLabel}>Second Subtitle Language</div>
+        <div className={controlLabel}>None loaded ?</div>
+      </div>
+      <div className={controlWrapper}>
+        <div className={controlLabel}>Space between captions</div>
+        <Toggle />
+      </div>
+      <div className={controlWrapper}>
+        <div className={controlLabel}>Custom text color</div>
+        <div className={flexbox}>
+          Red
+          <Toggle />
+        </div>
+      </div>
+      <div className={controlWrapper}>
+        <div className={controlLabel}>Small text</div>
+        <Toggle />
+      </div>
+      <div className={controlWrapper}>
+        <div className={controlLabel}>Hide action panel</div>
+        <Toggle />
+      </div>
+      <div className={controlWrapper}>
+        <div className={controlLabel}>UI Language</div>
+        <div className={controlLabel}>English</div>
+      </div>
+    </div>
   </div>
 );
-
-function withTimer(WrappedComponent) {
-  return class extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        time: 0
-      }
-    }
-
-    componentDidMount() {
-      this.timer = setInterval(() => {
-        this.setState({
-          time: Date.now()
-        });
-      }, 100);
-    }
-
-    componentWillUnmount() {
-      clearInterval(this.timer);
-    }
-
-    render() {
-      return (
-        <WrappedComponent
-         {...this.props}
-         time={this.state.time}
-        />
-      );
-    }
-  };
-}
-
-const Dialog = withTimer(props => {
-  const wrapperStyles = {
-    position: 'fixed',
-    zIndex: 2000,
-    top: 0,
-    left: 0,
-    width: '80%',
-    height: '100px',
-    background: 'white',
-    justifyContent: 'center',
-    display: 'flex',
-    alignItems: 'center',
-    margin: '5%',
-    padding: '5%',
-    textAlign: 'center'
-  };
-  const host = document.querySelector('.am-picker-popup-wrap');
-  const isSecondSubtitlePicker = document.querySelector('.am-picker-popup-title') && document.querySelector('.am-picker-popup-title').textContent === 'Second Subtitle Language';
-  if (host && isSecondSubtitlePicker) {
-    return ReactDOM.createPortal((
-      <div style={wrapperStyles}>
-        {props.children}
-      </div>
-    ), host);
-  } else {
-    return null;
-  }
-
-});
-
-class MainPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoadingTutorialOpen: false,
-      isLanguagePickerOpen: false,
-    };
-  }
-
-  render() {
-    const {
-      isLoadingTutorialOpen,
-      isLanguagePickerOpen
-    } = this.state;
-    return (
-      <Fragment>
-        <Modal
-          visible={isLoadingTutorialOpen}
-          onClose={() => {}}
-          transparent
-          title="Title"
-          footer={[{ text: 'Ok', onPress: () => { console.log('ok'); } }]}
-          wrapProps={{ onTouchStart: this.onWrapTouchStart }}
-          afterClose={() => { alert('afterClose'); }}
-        >
-          I'm the loading tutorial
-        </Modal>
-        <Dialog>
-          You'll need to load subtitles on the page to see them here.
-          <button onClick={() => {
-            this.setState({
-              isLanguagePickerOpen: false,
-            })
-          }}>
-            Show me
-          </button>
-        </Dialog>
-        <List
-          renderHeader={() => 'Dual Captions v2.3.4'}
-          extra=" "
-          >
-          <List.Item
-            extra={<Switch
-              checked
-            />}
-          >Turn on</List.Item>
-          <div onClick={() => {
-            this.setState({
-              isLanguagePickerOpen: true,
-            })
-          }}>
-            <Picker
-              data={[0, 0, 0]}
-              onOk={e => {
-                this.setState({
-                  isLanguagePickerOpen: false,
-                })
-              }}
-              onDismiss={() => {
-                this.setState({
-                  isLanguagePickerOpen: false,
-                })
-              }}
-              okText="Ok"
-              value={[0, 0, 0]}
-              extra=" "
-              dismissText="Dismiss"
-              title="Second Subtitle Language"
-              disabled={false}
-            >
-              <List.Item arrow="horizontal">Second Subtitle Language</List.Item>
-            </Picker>
-            <Picker
-              data={[0, 0, 0]}
-              onOk={e => {
-                this.setState({
-                  isLanguagePickerOpen: false,
-                })
-              }}
-              onDismiss={() => {
-                this.setState({
-                  isLanguagePickerOpen: false,
-                })
-              }}
-              okText="Ok"
-              value={[0, 0, 0]}
-              extra="🇺🇸"
-              dismissText="Dismiss"
-              title="UI Lang"
-              disabled={false}
-            >
-              <List.Item arrow="horizontal">UI Language</List.Item>
-            </Picker>
-          </div>
-        </List>
-      </Fragment>
-    )
-  }
-}
-
 
 export default MainPage;
